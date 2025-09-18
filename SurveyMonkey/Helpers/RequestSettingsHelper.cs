c3ea69a40c233a9a5ab4dc4d77d40a98ad0923c9
Patrick Bristow <patrick@thebristows.us>
2025-09-18 09:58:44 +0700

    Solved problem with CreateCollector kicking out a 400 error due to bools being formatted as strings

--- SurveyMonkey/Helpers/RequestSettingsHelper.cs
+++ SurveyMonkey/Helpers/RequestSettingsHelper.cs
@@ -3,53 +3,51 @@ using System.Collections.Generic;
 using System.Reflection;
 using SurveyMonkey.RequestSettings;
 
-namespace SurveyMonkey.Helpers
-{
-    internal static class RequestSettingsHelper
-    {
-        internal static RequestData GetPopulatedProperties(object obj)
-        {
-            var output = new RequestData();
-            foreach (PropertyInfo property in obj.GetType().GetProperties(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
-            {
-                if (property.GetValue(obj) != null)
-                {
-                    Type underlyingType = property.PropertyType.IsGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>)
-                        ? Nullable.GetUnderlyingType(property.PropertyType)
-                        : property.PropertyType;
-                    if (underlyingType.IsEnum)
-                    {
-                        output.Add(PropertyCasingHelper.CamelToSnake(property.Name), PropertyCasingHelper.CamelToSnake(property.GetValue(obj).ToString()));
-                    }
-                    else if (underlyingType == typeof(DateTime))
-                    {
-                        output.Add(PropertyCasingHelper.CamelToSnake(property.Name), ((DateTime)property.GetValue(obj)).ToString("s"));
-                    }
-                    else if (underlyingType == typeof(List<DateTime>))
-                    {
-                        output.Add(PropertyCasingHelper.CamelToSnake(property.Name), ((List<DateTime>)property.GetValue(obj)).ConvertAll(x => x.ToString("s")));
-                    }
-                    //SurveyMonkey uses strings to represent longs (eg for any Ids)
-                    else if (underlyingType == typeof(long))
-                    {
-                        output.Add(PropertyCasingHelper.CamelToSnake(property.Name), ((long)property.GetValue(obj)).ToString());
-                    }
-                    else if (underlyingType == typeof(List<long>))
-                    {
-                        output.Add(PropertyCasingHelper.CamelToSnake(property.Name), ((List<long>)property.GetValue(obj)).ConvertAll(x => x.ToString()));
-                    }
-                    else if (underlyingType == typeof(bool))
-                    {
-                        output.Add(PropertyCasingHelper.CamelToSnake(property.Name), PropertyCasingHelper.CamelToSnake(property.GetValue(obj).ToString()));
-                    }
-                    else
-                    {
-                        output.Add(PropertyCasingHelper.CamelToSnake(property.Name), property.GetValue(obj));
-                    }
+namespace SurveyMonkey.Helpers;
 
+internal static class RequestSettingsHelper
+{
+    internal static RequestData GetPopulatedProperties(object obj)
+    {
+        var output = new RequestData();
+        foreach (var property in obj.GetType().GetProperties(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public))
+        {
+            if (property.GetValue(obj) != null)
+            {
+                var underlyingType = property.PropertyType.IsGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>)
+                    ? Nullable.GetUnderlyingType(property.PropertyType)
+                    : property.PropertyType;
+                if (underlyingType.IsEnum)
+                {
+                    output.Add(PropertyCasingHelper.CamelToSnake(property.Name), PropertyCasingHelper.CamelToSnake(property.GetValue(obj).ToString()));
+                }
+                else if (underlyingType == typeof(DateTime))
+                {
+                    output.Add(PropertyCasingHelper.CamelToSnake(property.Name), ((DateTime)property.GetValue(obj)).ToString("s"));
+                }
+                else if (underlyingType == typeof(List<DateTime>))
+                {
+                    output.Add(PropertyCasingHelper.CamelToSnake(property.Name), ((List<DateTime>)property.GetValue(obj)).ConvertAll(x => x.ToString("s")));
+                }
+                //SurveyMonkey uses strings to represent longs (eg for any Ids)
+                else if (underlyingType == typeof(long))
+                {
+                    output.Add(PropertyCasingHelper.CamelToSnake(property.Name), ((long)property.GetValue(obj)).ToString());
+                }
+                else if (underlyingType == typeof(List<long>))
+                {
+                    output.Add(PropertyCasingHelper.CamelToSnake(property.Name), ((List<long>)property.GetValue(obj)).ConvertAll(x => x.ToString()));
+                }
+                else if (underlyingType == typeof(bool))
+                {
+                    output.Add(PropertyCasingHelper.CamelToSnake(property.Name), property.GetValue(obj));
+                }
+                else
+                {
+                    output.Add(PropertyCasingHelper.CamelToSnake(property.Name), property.GetValue(obj));
                 }
             }
-            return output;
         }
+        return output;
     }
 }
